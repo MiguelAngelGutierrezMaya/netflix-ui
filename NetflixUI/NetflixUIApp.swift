b703a88468e7cdb67bbe86ed81e0b942dc2c12c5
Miguel Angel <gutierrezmayamiguelangel@gmail.com>
2024-06-10 22:20:13 -0500

    Initial Commit

---
+++ NetflixUI/NetflixUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixUIApp.swift
+//  NetflixUI
+//
+//  Created by Miguel Angel Gutierrez Maya on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NetflixUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
